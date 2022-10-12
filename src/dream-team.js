f3524a6a5234688d876822491c8133f44e451202
Artem Kononenko <kononenkoar@yandex.ru>
2022-10-12 17:47:14 +0300

    feat: complete caunt-cats, dream-team, transform-array

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
   // remove line with error and write your code here
+
+  let arr = [];
+  let regEx = /[a-zA-Z]/;
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      arr.push(members[i].toUpperCase().match(regEx));
+    }
+  }
+  arr.sort();
+
+  return arr.join("").toUpperCase();
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,89 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  console.log(arguments);
+  let arrRes = [];
+  let count = 0;
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let arrClone = Array.from(arr);
+  let find = arrClone.find(
+    (el) =>
+      el == "--double-next" ||
+      el == "--double-prev" ||
+      el == "--discard-prev" ||
+      el == "--discard-next"
+  );
+
+  if (find === undefined) {
+    return arr;
+  }
+
+  if (arrClone[0] == "--discard-prev" || arrClone[0] == "--double-prev") {
+    arrRes = arrClone;
+    return arrRes.splice(1, arrRes.length - 1);
+  }
+  if (
+    arrClone[arrClone.length - 1] == "--double-next" ||
+    arrClone[arrClone.length - 1] == "--discard-next"
+  ) {
+    arrRes = arrClone;
+    return arrRes.splice(0, arrRes.length - 1);
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (Number(arrClone[i])) {
+      arrRes.push(arrClone[i]);
+    }
+    if (arrClone[i] == "--double-next") {
+      arrRes.push(arrClone[i + 1]);
+    }
+    if (arrClone[i] == "--double-prev") {
+      arrRes.push(arr[i - 1]);
+    }
+    if (arrClone[i] == "--discard-prev") {
+      arrRes.splice(i - 1, 1);
+    }
+    if (arrClone[i] == "--discard-next") {
+      count = i;
+    }
+  }
+  if (arrClone[count] == "--discard-next") {
+    arrRes.splice(count, 1);
+  }
+
+  if (
+    arrClone[count] == "--discard-next" &&
+    arrClone[count + 2] == "--double-prev"
+  ) {
+    arrRes.splice(count, 1);
+  }
+
+  if (
+    arrClone[count] == "--discard-next" &&
+    arrClone[count + 1] == "--discard-prev"
+  ) {
+    arrRes.splice(count, 2);
+  }
+  return arrRes;
 }
 
 module.exports = {
-  transform
+  transform,
 };
