5de0c018542c3584bf7d6a99f59e479e58a8d482
Artem Kononenko <kononenkoar@yandex.ru>
2022-10-13 13:47:07 +0300

    feat: complete encode-line

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  if (arr[i] == 0) {
+    count = i;
+    break;
+  } else if (arr[i] == 1) {
+    count = i;
+    break;
+  } else if (arr[i] == 2) {
+    count = i;
+    break;
+  } else if (arr[i] == 3) {
+    count = i;
+    break;
+  } else if (arr[i] == 4) {
+    count = i;
+    break;
+  } else if (arr[i] == 5) {
+    count = i;
+    break;
+  } else if (arr[i] == 6) {
+    count = i;
+    break;
+  } else if (arr[i] == 7) {
+    count = i;
+    break;
+  } else if (arr[i] == 8) {
+    count = i;
+    break;
+  } else if (arr[i] == 9) {
+    count = i;
+    break;
+  }
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
